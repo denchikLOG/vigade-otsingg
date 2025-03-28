99df124db201a00ca87e9f929d3207a329caace8
denchikLOG <deniss.melnikov@gmail.com>
2025-03-28 11:40:42 +0200

    Update PythonApplication3.py

--- vigade otsing/PythonApplication3.py
+++ vigade otsing/PythonApplication3.py
@@ -1,27 +1,45 @@
 import math
-pi=3.14
+
+# Ruudu karakteristikud
 print("Ruudu karakteristikud")
-a=int(input("Sisesta ruudu külje pikkus => "))
-S=a**2
-print("Ruudu pindala", S)
-P=4*a
-print("Ruudu ümbermõõt", P)
-di=a*math.sqrt(2)
-print("Ruudu diagonaal", round(di,2))
+try:
+    a=float(input('Sisesta ruudu külje pikkus=>'))
+    S=a**2
+    print("Ruudu pindala:",S)
+    P=4*a
+    print("Ruudu ümbermõõt:",P)
+    di=a*math.sqrt(2)
+    print("Ruudu diagonaal:",round(di,2))
+except ValueError:
+    print("Palun sisesta kehtiv arv ruudu külje pikkuseks.")
+
+print()
+
+# Ristküliku karakteristikud
 print("Ristküliku karakteristikud")
-b=int(input("Sisesta ristküliku 1. külje pikkus => "))
-c=int(input("Sisesta ristküliku 2. külje pikkus => "))
-S=b*c
-print("Ristküliku pindala", S)
-P=2*(b+c)
-print("Ristküliku ümbermõõt", P)
-di=math.sqrt(b*2+c*2)
-print("Ristküliku diagonaal", round(di))
+try:
+    b=float(input("Sisesta ristküliku 1. külje pikkus=>"))
+    c=float(input("Sisesta ristküliku 2. külje pikkus=>"))
+    S=b*c
+    print("Ristküliku pindala:",S)
+    P=2*(b+c)
+    print("Ristküliku ümbermõõt:",P)
+    di=math.sqrt(b**2+c**2)
+    print("Ristküliku diagonaal:",round(di,2))
+except ValueError:
+    print("Palun sisesta kehtivad arvud ristküliku külgede pikkuseks.")
+
+print()
+
+# Ringi karakteristikud
 print("Ringi karakteristikud")
-r=int(input("Sisesta ringi raadiusi pikkus => "))
-d=2*r
-print("Ringi läbimõõt", d)
-S=pi*r*2
-print("Ringi pindala", round(S))
-C=2*pi*r
-print("Ringjoone pikkus", round(C))
+try:
+    r=float(input("Sisesta ringi raadiusi pikkus=>"))
+    d=2*r
+    print("Ringi läbimõõt:",d)
+    S=math.pi*r**2
+    print("Ringi pindala:",round(S,2))
+    C=2*math.pi*r
+    print("Ringjoone pikkus:",round(C,2))
+except ValueError:
+    print("Palun sisesta kehtiv arv ringi raadiuse pikkuseks.")
