1d4466d43c51127efebc616daddb8435874ff20e
denchikLOG <deniss.melnikov@gmail.com>
2025-03-31 08:47:13 +0300

    Update PythonApplication3.py

--- vigade otsing/PythonApplication3.py
+++ vigade otsing/PythonApplication3.py
@@ -1,6 +1,4 @@
 import math
-
-# Ruudu karakteristikud
 print("Ruudu karakteristikud")
 try:
     a=float(input('Sisesta ruudu külje pikkus=>'))
@@ -10,12 +8,9 @@ try:
     print("Ruudu ümbermõõt:",P)
     di=a*math.sqrt(2)
     print("Ruudu diagonaal:",round(di,2))
-except ValueError:
+except Error:
     print("Palun sisesta kehtiv arv ruudu külje pikkuseks.")
-
 print()
-
-# Ristküliku karakteristikud
 print("Ristküliku karakteristikud")
 try:
     b=float(input("Sisesta ristküliku 1. külje pikkus=>"))
@@ -26,12 +21,9 @@ try:
     print("Ristküliku ümbermõõt:",P)
     di=math.sqrt(b**2+c**2)
     print("Ristküliku diagonaal:",round(di,2))
-except ValueError:
+except Error:
     print("Palun sisesta kehtivad arvud ristküliku külgede pikkuseks.")
-
 print()
-
-# Ringi karakteristikud
 print("Ringi karakteristikud")
 try:
     r=float(input("Sisesta ringi raadiusi pikkus=>"))
@@ -41,5 +33,5 @@ try:
     print("Ringi pindala:",round(S,2))
     C=2*math.pi*r
     print("Ringjoone pikkus:",round(C,2))
-except ValueError:
+except Error:
     print("Palun sisesta kehtiv arv ringi raadiuse pikkuseks.")
